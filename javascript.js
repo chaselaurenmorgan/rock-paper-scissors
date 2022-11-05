d6f33642cb201f461eafa1b939ee6f190f35194c
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-11-04 20:05:39 -0400

    add psuedocode for getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/*
+1. create a function name getComputerChoice
+    function getComputerChoice(){}
+2. will return a either 'Rock' , 'Paper', or 'Scissors' randomly 
+    use Math.random() to return the integer 1 to 3 
+    assign the numbers of 1,2,3, Rock Paper Scissors
+    let number = Math.floor(Math.random()*3)+ 1
+    if number === 1 => Rock
+    else if number ===2 => Paper
+    else => Scissors
+3. use the console to make sure that it works
+    console.log(getComputerChoice())
+*/
