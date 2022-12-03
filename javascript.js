b319d01179701035826491db9c3f5c40113b9456
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-12-02 20:51:47 -0500

    refactor 5 round functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let playerPoints = 0
+let computerPoints = 0
 /* Randomly returns the computer choice */
 const getComputerChoice = () =>{
   const randomChoice = ['rock','paper','scissors']
@@ -7,79 +9,50 @@ const getComputerChoice = () =>{
 
 /* Plays a single round of rps*/
 const playRound = (playerSelection,computerSelection) =>{
-    console.log('1',playerSelection,'2',computerSelection)
+    console.log('player:',playerSelection, 'computer:', computerSelection )
 
     if(playerSelection === computerSelection){
         return `It's a DRAW! You both chose ${playerSelection}`
     }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        playerPoints++
         return `You WIN! ${playerSelection} beats ${computerSelection}`
     }else if(computerSelection === 'rock' && playerSelection === 'scissors'){
+        computerPoints++
         return `You LOSE! ${computerSelection} beats ${playerSelection}`
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        playerPoints++
         return `You WIN! ${playerSelection} beats ${computerSelection}`
     }else if(computerSelection === 'paper' && playerSelection === 'rock'){
+        computerPoints++
         return `You LOSE! ${computerSelection} beats ${playerSelection}`
     }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        playerPoints++
         return `You WIN! ${playerSelection} beats ${computerSelection}`
     }else if(computerSelection === 'scissors' && playerSelection === 'paper'){
+        computerPoints++
         return `You LOSE! ${computerSelection} beats ${playerSelection}`
     }
 }
 
-const playerSelection = 'scissors'
-const computerChoice = getComputerChoice()
 
-console.log(playRound(playerSelection,computerChoice))
+/* Plays 5 rounds of rps*/
 
+function game(){
+    for(let i = 0 ; i < 5 ; i++){
+        const computerChoice = getComputerChoice()
+        const playerSelection = prompt('Chose your throw!', 'Rock,Paper,Scissors').toLowerCase()
+        console.log(playRound(playerSelection,computerChoice))
+        console.log('player:',playerPoints,'computer:',computerPoints)
+    }
+    if(playerPoints > computerPoints){
+        return `You WIN best out of 5!`
+    }else if(computerPoints > playerPoints){
+        return 'You LOSE best out of 5!'
+    }else {
+        return 'NO ONE WINS!'
+    }
+}
 
-
-
-
-
-/* 
-create a function name game()
-play 5 rounds using a loop 
-within that loop call the playRound()
-withing that loop keep score 
-report the loser or winner at the end
-*/
-
-// function game(){
-//     let playerPoints = 0
-//     let computerPoints = 0
-
-//     for(let i = 0; i < 5; i++){
-//         const playerSelection = prompt('Chose Rock, Paper, or Scissors').toLowerCase()
-//         const computerChoice = getComputerChoice()
-
-//         if(playRound(playerSelection,computerChoice) === 'win'){
-//             console.log(`${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats 
-//         ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}, You win!`)
-
-//             playerPoints++
-
-//         }else if (playRound(playerSelection,computerChoice) === 'lose'){
-
-//             console.log(`${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats 
-//             ${playerSelection[0].toUpperCase() + playerSelection.slice(1)}!, You lose!`)
-    
-//             computerPoints++
-            
-//         } else{
-//             console.log(`It a draw you both chose ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`)
-//         }
-        
-//         console.log(playerPoints,computerPoints)
-        
-//     }
-//      if(playerPoints > computerPoints){
-//         console.log('You Won the most out of 5 Rounds!')
-//      }else{
-//         console.log('You Loss the most out of 5 Rounds!')
-//      }
-
-// }
-
-
+console.log(game())
 
 
