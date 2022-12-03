bcadad9f105f23a0a6fea0f9f668e2d20bd2e96d
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-12-02 20:10:51 -0500

    refactor computer choice function

--- javascript.js
+++ javascript.js
@@ -1,28 +1,8 @@
-/*
-1. create a function name getComputerChoice
-    function getComputerChoice(){}
-2. will return a either 'Rock' , 'Paper', or 'Scissors' randomly 
-    use Math.random() to return the integer 1 to 3 
-    assign the numbers of 1,2,3, Rock Paper Scissors
-    let number = Math.floor(Math.random()*3)+ 1
-    if number === 1 => Rock
-    else if number ===2 => Paper
-    else => Scissors
-3. use the console to make sure that it works
-    console.log(getComputerChoice())
-*/
-
-function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random()*3)+1
-    
-    if(randomNumber === 1){
-        return 'rock'
-    }else if(randomNumber === 2){
-        return 'paper'
-    }else {
-        return 'scissors'
-    }
-    
+/* Randomly returns the computer choice */
+const getComputerChoice = () =>{
+  const randomChoice = ['rock','paper','scissors']
+  let randomNumber = Math.floor(Math.random()* 3)
+    return randomChoice[randomNumber]
 }
 
 /* 
