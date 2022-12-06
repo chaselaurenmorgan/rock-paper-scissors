ccfe14cb03cfa165b703e5e11deffd381d8d5b1e
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-12-05 19:58:37 -0500

    refactor buttons to have one eventlister

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 let playerPoints = 0
 let computerPoints = 0
+const outcome = document.querySelector('.outcome')
+const score = document.querySelector('#score')
+const buttons = document.querySelectorAll('button')
+
+
 /* Randomly returns the computer choice */
 const getComputerChoice = () =>{
   const randomChoice = ['rock','paper','scissors']
@@ -9,50 +14,48 @@ const getComputerChoice = () =>{
 
 /* Plays a single round of rps*/
 const playRound = (playerSelection,computerSelection) =>{
-    console.log('player:',playerSelection, 'computer:', computerSelection )
 
     if(playerSelection === computerSelection){
-        return `It's a DRAW! You both chose ${playerSelection}`
+        outcome.textContent = `It's a DRAW! You both chose ${playerSelection}`
     }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         playerPoints++
-        return `You WIN! ${playerSelection} beats ${computerSelection}`
+        outcome.textContent =`You WIN! ${playerSelection} beats ${computerSelection}`
     }else if(computerSelection === 'rock' && playerSelection === 'scissors'){
         computerPoints++
-        return `You LOSE! ${computerSelection} beats ${playerSelection}`
+        outcome.textContent =`You LOSE! ${computerSelection} beats ${playerSelection}`
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
         playerPoints++
-        return `You WIN! ${playerSelection} beats ${computerSelection}`
+        outcome.textContent =`You WIN! ${playerSelection} beats ${computerSelection}`
     }else if(computerSelection === 'paper' && playerSelection === 'rock'){
         computerPoints++
-        return `You LOSE! ${computerSelection} beats ${playerSelection}`
+        outcome.textContent = `You LOSE! ${computerSelection} beats ${playerSelection}`
     }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
         playerPoints++
-        return `You WIN! ${playerSelection} beats ${computerSelection}`
+        outcome.textContent = `You WIN! ${playerSelection} beats ${computerSelection}`
     }else if(computerSelection === 'scissors' && playerSelection === 'paper'){
         computerPoints++
-        return `You LOSE! ${computerSelection} beats ${playerSelection}`
+        outcome.textContent = `You LOSE! ${computerSelection} beats ${playerSelection}`
     }
+    
+    score.textContent = `YOUR SCORE:${playerPoints}  COMPUTER SCORE:${computerPoints}`
+
+    if(playerPoints === 5){
+        outcome.textContent = 'GAME OVER: YOU WIN FIRST ONE TO 5PTS!'
+    }else if(computerPoints === 5){
+        outcome.textContent = 'GAME OVER: YOU LOSE - COMPUTER WAS THE FIRST ONE TO 5PTS!'
+    }
+
+
 }
 
-
-/* Plays 5 rounds of rps*/
-
-function game(){
-    for(let i = 0 ; i < 5 ; i++){
+buttons.forEach(button => {
+    button.addEventListener('click',()=> {
         const computerChoice = getComputerChoice()
-        const playerSelection = prompt('Chose your throw!', 'Rock,Paper,Scissors').toLowerCase()
-        console.log(playRound(playerSelection,computerChoice))
-        console.log('player:',playerPoints,'computer:',computerPoints)
-    }
-    if(playerPoints > computerPoints){
-        return `You WIN best out of 5!`
-    }else if(computerPoints > playerPoints){
-        return 'You LOSE best out of 5!'
-    }else {
-        return 'NO ONE WINS!'
-    }
-}
+        const playerSelection = `${button.className}`
+         playRound(playerSelection,computerChoice)
+    })
+})
 
-console.log(game())
+
 
 
